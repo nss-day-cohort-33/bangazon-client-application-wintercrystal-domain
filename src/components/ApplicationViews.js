fc2e99ecbffb34cd60513f3327acbff6cc5b922b
Tyler Carpenter <tcarp55@gmail.com>
2019-10-03 15:29:25 -0500

    added auth, nav and applicationviews

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,65 @@
+import { Route } from "react-router-dom"
+import React from "react"
+import { withRouter } from "react-router-dom"
+import Register from "./auth/Register"
+import Login from "./auth/Login"
+
+
+
+const ApplicationViews = () => {
+    return (
+        <React.Fragment>
+
+            <Route
+                exact path="/" render={props => {
+                    return <ParkExplorer {...props} />
+                }}
+            />
+
+            <Route
+                path="/register" render={props => {
+                    return <Register {...props} />
+                }}
+            />
+
+            <Route
+                path="/login" render={props => {
+                    return <Login {...props} />
+                }}
+            />
+
+            <Route
+                path="/areas" render={props => {
+                    return (
+                        <>
+                            <h1>Areas</h1>
+                            <img className="swings" src={require('./home/swings.jpeg')} alt="My Dog" />
+                        </>
+                    )
+                }}
+            />
+
+            <Route
+                path="/attractions" render={props => {
+                    return (
+                        <>
+                            <h1>Attractions</h1>
+                            <img className="swings" src={require('./home/swings.jpeg')} alt="My Dog" />
+                        </>
+                    )
+                }}
+            />
+
+            <Route
+                path="/myitinerary" render={props => {
+                    return (
+                       <MyItinerary/>
+                    )
+                }}
+            />
+
+        </React.Fragment>
+    )
+}
+
+export default withRouter(ApplicationViews)
