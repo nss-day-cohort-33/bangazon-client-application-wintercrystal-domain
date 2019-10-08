1f8a1189ada9e147c7fdb8da16aa80f05d4c0412
Daniel Krusch <daniel.krusch@gmail.com>
2019-10-08 15:20:13 -0500

    Added comments to product detail

--- src/components/products/ProductDetail.js
+++ src/components/products/ProductDetail.js
@@ -2,14 +2,21 @@ import React, { useEffect, useState } from "react"
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth"
 
 
+// By Daniel Krusch
 const ProductDetail = props => {
+    // Order will contain open orders for this user (i.e ones with payment type of null)
     const [order, setOrder] = useState([])
+    // Order Product will contain an order product relation row if it exists
     const [orderProduct, setOrderProducts] = useState([])
     const { isAuthenticated } = useSimpleAuth()
 
+    // For the created date field in order
     let datestring = new Date().toISOString().slice(0,10)
 
+    // Follows getOrders, will set a new order
     const getOrderProducts = (data) => {
+            // If there exists an open order, then we will fetch the order_product relation for that order
+            // if there isnt an open order we'll just set the order and leave orderProduct empty
             if (data.length !== 0)
             {
                 console.log(data)
@@ -28,6 +35,7 @@ const ProductDetail = props => {
             }
     }
 
+    // First we get the open orders and then call getOrderProducts
     const getOrders = () => {
         if (isAuthenticated()) {
             fetch(`http://localhost:8000/orders?customer_id=${localStorage.getItem("id")}`, {
@@ -41,11 +49,15 @@ const ProductDetail = props => {
         }
     }
 
-
+    // On mount get some orders
     useEffect(getOrders, [])
 
+    // Will post orders and order products on click of the add to order button
     const addOrder = () => {
+        // Checks that user is valid
         if (isAuthenticated()) {
+            // If an open order does not exist one will be created, then an order_product relation will be created
+            // with an order_id of the order just created and a product id of the current product
             if (order.length === 0) {
                 console.log("hello")
                 fetch(`http://localhost:8000/orders`, {
@@ -79,7 +91,10 @@ const ProductDetail = props => {
                     .then(getOrders)
                 })
             }
+            // If an open order already exists we don't have to create one, we can just create the order_product
+            // relation
             else {
+                // If an order product relation does not exist, make one
                 if (orderProduct.length === 0)
                 {
                     console.log("goodbye")
@@ -97,6 +112,7 @@ const ProductDetail = props => {
                         })
                     })
                 }
+                // If an order product relation does exist update the quantity on click of add to order
                 else {
                     fetch(`http://localhost:8000/orderproducts/${orderProduct[0].id}`, {
                         "method": "PUT",
@@ -116,7 +132,6 @@ const ProductDetail = props => {
     return (
         <>
             {
-
                 <section className="product-details">
                     <h3>{props.product.name}</h3>
                     <h4>{props.product.price.toFixed(2)}</h4>
